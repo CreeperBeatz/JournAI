75e847398ece3b213484211cb95073eb4a01a36c
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-21 21:06:08 +0300

    fix: remove salt saving

--- database/db_utils.py
+++ database/db_utils.py
@@ -5,77 +5,69 @@ from modules.config_manager import SALT
 
 import bcrypt
 
-engine = create_engine('sqlite:///journal_app.db')  # Create SQLite database
-Session = sessionmaker(bind=engine)
-session = Session()
+class DBManager:
 
+    # Initialize the database
+    def __init__(self):
+        engine = create_engine('sqlite:///journal_app.db')  # Create SQLite database
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        Base.metadata.create_all(engine)
 
-# Initialize the database
-def init_db():
-    Base.metadata.create_all(engine)
+        self.salt = bcrypt.gensalt()
 
+    # User related operations
+    def add_user(self, username, password):
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), self.salt)
+        user = User(username=username, password=hashed_password)
+        self.session.add(user)
+        self.session.commit()
 
-# User related operations
-def add_user(username, password):
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), SALT)
-    user = User(username=username, password=hashed_password)
-    session.add(user)
-    session.commit()
+    def verify_login(self, username, password):
+        user = self.session.query(User).filter_by(username=username).first()
+        if user:
+            return bcrypt.checkpw(password.encode('utf-8'), user.password)
+        return False
 
+    # Journal Entry related operations
+    def save_journal_entry(self, user_id, question_id, answer):
+        entry = JournalEntry(user_id=user_id, question_id=question_id, answer=answer)
+        self.session.add(entry)
+        self.session.commit()
 
-def verify_login(username, password):
-    user = session.query(User).filter_by(username=username).first()
-    if user:
-        return bcrypt.checkpw(password.encode('utf-8'), user.password)
-    return False
+    # Weekly Summary related operations
+    def save_weekly_summary(self, user_id, summary):
+        weekly_summary = WeeklySummary(user_id=user_id, summary=summary)
+        self.session.add(weekly_summary)
+        self.session.commit()
 
+    # Monthly Summary related operations
+    def save_monthly_summary(self, user_id, summary):
+        monthly_summary = MonthlySummary(user_id=user_id, summary=summary)
+        self.session.add(monthly_summary)
+        self.session.commit()
 
-# Journal Entry related operations
-def save_journal_entry(user_id, question_id, answer):
-    entry = JournalEntry(user_id=user_id, question_id=question_id, answer=answer)
-    session.add(entry)
-    session.commit()
+    # Question related operations
+    def add_question_to_user(self, user_id, question_text):
+        question = Question(user_id=user_id, question_text=question_text)
+        self.session.add(question)
+        self.session.commit()
 
+    def delete_question(self, user_id, question_id):
+        question = self.session.query(Question).filter_by(user_id=user_id, id=question_id).first()
+        if question:
+            self.session.delete(question)
+            self.session.commit()
 
-# Weekly Summary related operations
-def save_weekly_summary(user_id, summary):
-    weekly_summary = WeeklySummary(user_id=user_id, summary=summary)
-    session.add(weekly_summary)
-    session.commit()
+    def get_daily_questions_for_user(self, user_id):
+        questions = session.query(Question).filter_by(user_id=user_id).all()
+        return [q.question_text for q in questions]
 
+    # History related operations
+    def get_weekly_summaries(self, user_id):
+        summaries = session.query(WeeklySummary).filter_by(user_id=user_id).all()
+        return [s.summary for s in summaries]
 
-# Monthly Summary related operations
-def save_monthly_summary(user_id, summary):
-    monthly_summary = MonthlySummary(user_id=user_id, summary=summary)
-    session.add(monthly_summary)
-    session.commit()
-
-
-# Question related operations
-def add_question_to_user(user_id, question_text):
-    question = Question(user_id=user_id, question_text=question_text)
-    session.add(question)
-    session.commit()
-
-
-def delete_question(user_id, question_id):
-    question = session.query(Question).filter_by(user_id=user_id, id=question_id).first()
-    if question:
-        session.delete(question)
-        session.commit()
-
-
-def get_daily_questions_for_user(user_id):
-    questions = session.query(Question).filter_by(user_id=user_id).all()
-    return [q.question_text for q in questions]
-
-
-# History related operations
-def get_weekly_summaries(user_id):
-    summaries = session.query(WeeklySummary).filter_by(user_id=user_id).all()
-    return [s.summary for s in summaries]
-
-
-def get_monthly_summaries(user_id):
-    summaries = session.query(MonthlySummary).filter_by(user_id=user_id).all()
-    return [s.summary for s in summaries]
+    def get_monthly_summaries(self, user_id):
+        summaries = session.query(MonthlySummary).filter_by(user_id=user_id).all()
+        return [s.summary for s in summaries]

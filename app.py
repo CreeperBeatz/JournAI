f1dcd0ccc2ecff43caeb78e3702e0865dfff8626
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-21 20:50:27 +0300

    feat: add basic app functionality

--- app.py
+++ app.py
@@ -1,16 +1,45 @@
-# This is a sample Python script.
+import streamlit as st
+from database.db_utils import add_user, add_question_to_user, delete_question, get_daily_questions_for_user, \
+    verify_login
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Initialize Streamlit
+st.title("Daily Journal App")
 
+menu = ["Home", "Login", "SignUp"]
+choice = st.selectbox("Menu", menu)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+if choice == "Home":
+    st.subheader("Home")
 
+elif choice == "Login":
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+    if st.button("Login"):
+        login_result = verify_login(username, password)
+        if login_result:
+            st.success("Logged In as {}".format(username))
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+            user_id = ...  # Retrieve the user's ID from the database
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+            # Display daily questions
+            questions = get_daily_questions_for_user(user_id)
+            answers = {}
+            for q in questions:
+                answers[q] = st.text_input(q)
+
+            if st.button("Submit Journal Entry"):
+                # Pass the answers to get the summary
+                #summary = get_summary(answers)
+                st.write("Your Summary: ", "summary")
+
+        else:
+            st.warning("Incorrect Username/Password")
+
+elif choice == "SignUp":
+    st.subheader("Create a New Account")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+
+    if st.button("Signup"):
+        add_user(username, password)
+        st.success("You have successfully created an account.")

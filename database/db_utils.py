c6213505b64dd7442839fe71b9a9fc67a4b03262
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-21 20:34:56 +0300

    feat: bind questions to users

--- database/db_utils.py
+++ database/db_utils.py
@@ -51,21 +51,21 @@ def save_monthly_summary(user_id, summary):
 
 
 # Question related operations
-def add_question(question_text):
-    question = Question(question_text=question_text)
+def add_question_to_user(user_id, question_text):
+    question = Question(user_id=user_id, question_text=question_text)
     session.add(question)
     session.commit()
 
 
-def delete_question(question_id):
-    question = session.query(Question).filter_by(id=question_id).first()
+def delete_question(user_id, question_id):
+    question = session.query(Question).filter_by(user_id=user_id, id=question_id).first()
     if question:
         session.delete(question)
         session.commit()
 
 
-def get_daily_questions():
-    questions = session.query(Question).all()  # Assuming all questions are daily for this example
+def get_daily_questions_for_user(user_id):
+    questions = session.query(Question).filter_by(user_id=user_id).all()
     return [q.question_text for q in questions]
 
 
--- database/models.py
+++ database/models.py
@@ -31,8 +31,11 @@ class Question(Base):
     __tablename__ = 'questions'
 
     id = Column(Integer, primary_key=True)
-    question_text = Column(Text, nullable=False)
+    question_text = Column(String(255), nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
 
+    # New Relationship
+    user = relationship('User', back_populates='questions')
 
 class JournalEntry(Base):
     __tablename__ = 'journal_entries'

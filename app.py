7685ffdaf6b3f43752ce3f8b7839132633240821
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-21 21:05:57 +0300

    fix: remove non-working functionality

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
-from database.db_utils import add_user, add_question_to_user, delete_question, get_daily_questions_for_user, \
-    verify_login
+from database.db_utils import DBManager
 
 # Initialize Streamlit
 st.title("Daily Journal App")
 
-menu = ["Home", "Login", "SignUp"]
+menu = ["Login", "SignUp"]
 choice = st.selectbox("Menu", menu)
+
+db_manager = DBManager()
 
 if choice == "Home":
     st.subheader("Home")
@@ -15,23 +16,10 @@ elif choice == "Login":
     username = st.text_input("Username")
     password = st.text_input("Password", type='password')
     if st.button("Login"):
-        login_result = verify_login(username, password)
+        login_result = db_manager.verify_login(username, password)
         if login_result:
             st.success("Logged In as {}".format(username))
-
-            user_id = ...  # Retrieve the user's ID from the database
-
-            # Display daily questions
-            questions = get_daily_questions_for_user(user_id)
-            answers = {}
-            for q in questions:
-                answers[q] = st.text_input(q)
-
-            if st.button("Submit Journal Entry"):
-                # Pass the answers to get the summary
-                #summary = get_summary(answers)
-                st.write("Your Summary: ", "summary")
-
+            # TODO Display daily questions
         else:
             st.warning("Incorrect Username/Password")
 
@@ -41,5 +29,5 @@ elif choice == "SignUp":
     password = st.text_input("Password", type='password')
 
     if st.button("Signup"):
-        add_user(username, password)
+        db_manager.add_user(username, password)
         st.success("You have successfully created an account.")

4b3252a8ce12de87d9776594cd91c4921b93c86f
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-21 21:08:24 +0300

    fix: remove SALT param

--- database/db_utils.py
+++ database/db_utils.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database.models import Base, User, Question, JournalEntry, WeeklySummary, MonthlySummary
-from modules.config_manager import SALT
 
 import bcrypt
 
--- modules/config_manager.py
+++ modules/config_manager.py
@@ -3,5 +3,4 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.cfg')
 
-SALT = config['Security']['SALT']
 OPENAI_API_KEY = config['OpenAI']['API_KEY']

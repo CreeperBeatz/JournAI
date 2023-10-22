73a92c9ad1ad0b5b1c4d973694383412d56d11d8
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-22 11:04:42 +0300

    feat: add global page config param

--- modules/config_manager.py
+++ modules/config_manager.py
@@ -4,3 +4,7 @@ config = configparser.ConfigParser()
 config.read('config.cfg')
 
 OPENAI_API_KEY = config['OpenAI']['API_KEY']
+
+# Global params
+PAGE_CONFIG = {"page_title": "JournAI",
+               "page_icon": "logo.png"}

ab3ba77ed003cdf3cece429ae52c78763ec1998f
dani_cosmosthrace <dani.matev123@gmail.com>
2023-10-25 23:59:12 +0300

    refactor: follow early escape code writing for daily_entry.py

--- pages/daily_entry.py
+++ pages/daily_entry.py
@@ -16,38 +16,48 @@ if not user_id:
 
 db_manager = DBManager()
 daily_questions: List[Question] = db_manager.get_questions_for_user(user_id)
-
-if daily_questions:
-    st.title("Daily Questions")
-
-    with st.form(key='daily_questions_form'):
-        for question in daily_questions:
-            # Skip deleted questions
-            if question.is_deleted:
-                continue
-
-            st.subheader(question.question_text)
-            existing_entry = db_manager.get_journal_entry(user_id, question.id)
-
-            # prefill the answer field with existing answer if any
-            answer = st.text_input(f"Answer for {question.question_text}",
-                                   existing_entry.answer if existing_entry else "")
-
-            # To store the new answer for each question
-            st.session_state[f"{question.id}_new_answer"] = answer
-
-        # When the user presses the "Submit" button, the answers get updated or created in the database
-        submitted = st.form_submit_button("Submit Answers")
-
-        if submitted:
-            for question in daily_questions:
-                if question.is_deleted:
-                    continue
-
-                new_answer = st.session_state.get(f"{question.id}_new_answer", None)
-
-                if new_answer is not None:
-                    db_manager.save_journal_entry(user_id, question.id, new_answer)
-                    st.success(f"Answer for {question.question_text} has been saved.")
-else:
+if not daily_questions:
     st.info("No questions available for today.")
+    st.stop()
+
+st.title("Daily Questions")
+
+any_saved = False  # Flag to check if any answer was saved or updated
+
+with st.form(key='daily_questions_form'):
+    for question in daily_questions:
+        if question.is_deleted:
+            continue
+
+        st.subheader(question.question_text)
+
+        existing_entry = db_manager.get_journal_entry(user_id, question.id)
+
+        if existing_entry:
+            initial_answer = existing_entry.answer
+        else:
+            initial_answer = ""
+
+        # Allow for multiline text
+        answer = st.text_area(f"Answer for {question.question_text}", initial_answer)
+
+        # Store the new answer for each question
+        st.session_state[f"{question.id}_new_answer"] = answer
+
+    if not st.form_submit_button("Submit Answers"):
+        st.stop()
+
+    for question in daily_questions:
+        if question.is_deleted:
+            continue
+
+        new_answer = st.session_state.get(f"{question.id}_new_answer", None)
+
+        if new_answer is None:
+            continue
+
+        db_manager.save_journal_entry(user_id, question.id, new_answer)
+        any_saved = True  # Set the flag to true if any answer is saved or updated
+
+    if any_saved:
+        st.success("Your answers have been successfully saved.")

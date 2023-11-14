a77ace92590cbeb2ec48bf79b4c1df23d403ef51
dani_cosmosthrace <dani.matev123@gmail.com>
2023-11-14 08:51:03 +0200

    feat: period picker

--- modules/streamlit_helper.py
+++ modules/streamlit_helper.py
@@ -1,4 +1,15 @@
+import calendar
+from enum import Enum
+
 from st_pages import Page, show_pages
+import streamlit as st
+import datetime
+
+
+class PeriodOptions(Enum):
+    WEEK = 1,
+    MONTH = 2,
+    YEAR = 3,
 
 
 def setup_pages_no_login():
@@ -13,7 +24,47 @@ def setup_pages_with_login():
         Page("app.py", "Home", "🎃"),
         Page("pages/my_account.py", "My Account", "🦱"),
         Page("pages/daily_entry.py", "Daily Entry", "✒️"),
-        Page("pages/weekly_summary.py", "Week Review", "📃"),
-        Page("pages/monthly_summary.py", "Month Review", "📰"),
-        Page("pages/yearly_summary.py", "Year Review", "📖"),
+        Page("pages/week_review.py", "Week Review", "📃"),
+        Page("pages/month_review.py", "Month Review", "📰"),
+        Page("pages/year_review.py", "Year Review", "📖"),
     ])
+
+
+def period_picker(label: str = "Select a date",
+                  period: PeriodOptions = PeriodOptions.WEEK,
+                  start_date_limit: datetime.date = None,
+                  end_date_limit: datetime.date = None,
+                  ):
+
+    if start_date_limit and end_date_limit:
+        selected_date = st.date_input(label, min_value=start_date_limit, max_value=end_date_limit)
+    elif start_date_limit:
+        selected_date = st.date_input(label, min_value=start_date_limit)
+    elif end_date_limit:
+        selected_date = st.date_input(label, max_value=end_date_limit)
+    else:
+        selected_date = st.date_input(label)
+
+    if period == PeriodOptions.WEEK:
+        start_date = selected_date - datetime.timedelta(days=selected_date.weekday())
+        end_date = start_date + datetime.timedelta(days=6)
+        month_year = selected_date.strftime("%B %Y")
+        period_str = f"Week {selected_date.isocalendar()[1]}, {month_year}"
+
+    elif period == PeriodOptions.MONTH:
+        start_date = selected_date.replace(day=1)
+        last_day = calendar.monthrange(selected_date.year, selected_date.month)[1]
+        end_date = selected_date.replace(day=last_day)
+        period_str = selected_date.strftime("%B %Y")
+
+    elif period == PeriodOptions.YEAR:
+        start_date = datetime.date(selected_date.year, 1, 1)
+        end_date = datetime.date(selected_date.year, 12, 31)
+        period_str = str(selected_date.year)
+
+    else:
+        raise ValueError("Invalid period option")
+
+    return start_date, end_date, period_str
+
+# Further logic can go here, using the week_start and week_end as needed

39dc5ea64ea11e4d548a99eaf785df49dd690974
dani_cosmosthrace <dani.matev123@gmail.com>
2023-11-14 08:50:55 +0200

    refactor: rename files to more descriptive ones

--- pages/week_review.py
+++ pages/week_review.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from database.db_utils import DBManager
 from modules.config_manager import PAGE_CONFIG
-from modules.streamlit_helper import setup_pages_with_login
+from modules.streamlit_helper import setup_pages_with_login, period_picker, PeriodOptions
 from modules.utilities import ExtendedEnum
 
 # Initialize Streamlit
@@ -13,6 +13,8 @@ class MenuOptions(ExtendedEnum):
     mood_review = "Mood Review"
     insights = "Insights"
 
+week_start, week_end, chosen_week = period_picker("Choose a week", PeriodOptions.WEEK)
+st.markdown(f"You have chosen: *{chosen_week}*")
 
 choice = st.selectbox("Select Analysis", MenuOptions.values(), label_visibility='visible')
 

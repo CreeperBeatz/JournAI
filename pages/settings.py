20ece5033edb6e59b827b535e6aed9075405f6b9
dani_cosmosthrace <dani.matev123@gmail.com>
2024-04-15 19:23:09 +0300

    feat: show daily questions in the settings page

--- pages/settings.py
+++ pages/settings.py
@@ -4,7 +4,7 @@ from modules.config import PAGE_CONFIG
 import yaml
 from yaml.loader import SafeLoader
 import modules.streamlit_helper as sthelper
-
+import modules.question_manager as question_manager
 
 # Initialize Streamlit
 st.set_page_config(**PAGE_CONFIG)
@@ -35,3 +35,15 @@ with open("config.yaml") as file:
 
 with open("config.yaml", "w") as file:
     yaml.dump(config, file, default_flow_style=False)
+
+with st.container(border=True):
+    user_questions = question_manager.get_questions(username)
+    if user_questions:
+        st.markdown("#### Your daily questions are as follows:")
+        questions_list = '* ' + '\n * '.join(user_questions)
+        st.markdown(questions_list)
+        st.markdown("\nIf you want to change your daily questions, ask the chatbot to do so!")
+    else:
+        st.markdown("#### You haven't set up your daily questions yet!\n\n Ask the Chatbot to set them "
+                    "up for you, by going to the Home page and writing 'Help me set up my daily "
+                    "questions'.")
